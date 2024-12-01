62f1df458d63f9cafdb9ebaa3605f2aa5bdcb2d6
merrywhether <r@risto.io>
2024-12-01 10:50:17 -0800

    add tests and improve scaffold

--- src/00/main.ts
+++ src/00/main.ts
@@ -1,1 +1,10 @@
-console.log("hello template");
+export async function main(target = "input") {
+  const dirpath = new URL(".", import.meta.url).pathname;
+  const text = await Deno.readTextFile(`${dirpath}${target}.txt`);
+
+  return { text };
+}
+
+if (import.meta.main) {
+  console.log(main());
+}
--- src/01/main.ts
+++ src/01/main.ts
@@ -1,35 +1,35 @@
-// const target = "sample";
-// https://adventofcode.com/2024/day/1/input
-const target = "input";
+export async function main(target: string) {
+  const dirpath = new URL(".", import.meta.url).pathname;
+  const text = await Deno.readTextFile(`${dirpath}/${target}.txt`);
 
-const dirpath = new URL(".", import.meta.url).pathname;
-const text = await Deno.readTextFile(`${dirpath}/${target}.txt`);
-
-const data = text.split("\n").reduce((agg, line) => {
-  const match = line.match(/^(\d+)\s+(\d+)$/);
-  if (match) {
-    agg.left.push(Number(match[1]));
-    agg.right.push(Number(match[2]));
-  }
-  return agg;
-}, { left: [] as number[], right: [] as number[] });
-
-data.left.sort();
-data.right.sort();
-
-const stats = data.right.reduce((agg, val) => {
-  agg[val] ??= 0;
-  agg[val] += 1;
-  return agg;
-}, [] as number[]);
-
-const result = data.left.reduce(
-  (agg, val, idx) => {
-    agg.dist = agg.dist + Math.abs(data.right[idx] - val);
-    agg.sim = agg.sim + val * (stats[val] ?? 0);
+  const data = text.split("\n").reduce((agg, line) => {
+    const match = line.match(/^(\d+)\s+(\d+)$/);
+    if (match) {
+      agg.left.push(Number(match[1]));
+      agg.right.push(Number(match[2]));
+    }
     return agg;
-  },
-  { dist: 0, sim: 0 },
-);
+  }, { left: [] as number[], right: [] as number[] });
 
-console.log(result);
+  data.left.sort();
+  data.right.sort();
+
+  const stats = data.right.reduce((agg, val) => {
+    agg[val] ??= 0;
+    agg[val] += 1;
+    return agg;
+  }, [] as number[]);
+
+  return data.left.reduce(
+    (agg, val, idx) => {
+      agg.dist = agg.dist + Math.abs(data.right[idx] - val);
+      agg.sim = agg.sim + val * (stats[val] ?? 0);
+      return agg;
+    },
+    { dist: 0, sim: 0 },
+  );
+}
+
+if (import.meta.main) {
+  console.log(await main("input"));
+}
